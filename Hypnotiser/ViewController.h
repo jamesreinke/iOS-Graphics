8c5f3a2f097819beeb986cd067264d17270cea09
jamesreinke <james@rtsystems.io>
2015-08-17 10:38:25 -0700

    Initial Commit

---
+++ Hypnotiser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnotiser
+//
+//  Created by James Reinke on 8/17/15.
+//  Copyright (c) 2015 Twenty Two Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
