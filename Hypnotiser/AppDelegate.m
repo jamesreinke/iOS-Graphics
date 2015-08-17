60911a68c32fcec42a831bf86dfc781b30b9fbe9
jamesreinke <james@rtsystems.io>
2015-08-17 13:53:11 -0700

    ~

--- Hypnotiser/AppDelegate.m
+++ Hypnotiser/AppDelegate.m
@@ -19,10 +19,28 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    // first subview
-    CGRect firstFrame = self.window.bounds; // make the frame fill the bounds of its container
-    HypnosisView *firstView = [[HypnosisView alloc] initWithFrame:firstFrame];
-    [self.window addSubview:firstView];
+    // Create CGRects for frames
+    CGRect screenRect = self.window.bounds;
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2.0;
+    
+    
+    // Create a screen-sized scroll view and add it to the window
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    scrollView.pagingEnabled = YES;
+    [self.window addSubview:scrollView];
+    
+    // Create a screen-sized hypnosis view and add it to the scoll view
+    HypnosisView *hypnosisView = [[HypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:hypnosisView];
+    
+    // Add a second screen-sized hypnosis view just off the screen to the right
+    screenRect.origin.x += screenRect.size.width;
+    HypnosisView *anotherView = [[HypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherView];
+    
+    // Tell the scroll view how big its content area is
+    scrollView.contentSize = bigRect.size;
     
     
     self.window.backgroundColor = [UIColor whiteColor];
